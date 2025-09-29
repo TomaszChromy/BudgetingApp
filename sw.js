78a11919f610d117b53a7dd6c4d9aef8c5a68bcb
Tomasz Chromy <twoj@mail.com>
2025-09-29 14:49:58 +0200

    feat: multiwallet + waluty + CSV(all) + Print + PWA (v8)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'budgetingapp-v3';
+﻿const CACHE = 'budgetingapp-v8';
 const ASSETS = [
   './','./index.html','./manifest.webmanifest',
   './assets/icon-192.png','./assets/icon-512.png',
@@ -8,7 +8,7 @@ self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))) .then(()=> self.clients.claim()));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(()=> self.clients.claim()));
 });
 self.addEventListener('fetch', e => {
   e.respondWith(
@@ -26,3 +26,5 @@ self.addEventListener('fetch', e => {
     }))
   );
 });
+
+
